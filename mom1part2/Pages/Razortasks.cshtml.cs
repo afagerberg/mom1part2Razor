260b2ad95f2eb58c72f3d8a1d4af78a8227dc4a0
afagerberg <alicefagerberg96@gmail.com>
2022-01-29 19:08:25 +0100

    Add project files.

---
+++ mom1part2/Pages/Razortasks.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace mom1part2.Pages
+{
+    public class RazortasksModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ mom1part2/wwwroot/js/site.js
@@ -0,0 +1,35 @@
+﻿// DT191G moment 1 del 2, Alice Fagerberg
+"use strict";
+
+// Navigering - märk den aktiva navlänken
+const current = location.href;
+const navlinks = document.querySelectorAll("nav a");
+
+for (let i = 0; i < navlinks.length; i++) {
+    if (navlinks[i].href === current) {
+        navlinks[i].className = "active";
+    }
+}
+
+// Klocka.
+function digitalClock() {
+
+    let date = new Date();
+    let h = date.getHours();
+    let m = date.getMinutes();
+    let s = date.getSeconds();
+
+    if (h < 10) h = "0" + h;
+    if (m < 10) m = "0" + m;
+    if (s < 10) s = "0" + s;
+
+    let time = h + ":" + m + ":" + s;
+
+    document.getElementById("timebox").innerHTML = "<p>" + time + "</p>";
+    setTimeout(digitalClock, 1000);
+
+    
+}
+
+digitalClock();
+
